fd59330c414909714f28d5d7c2afc7dfb23de2bb
Shusmoy Chowdhury <1305108.sc@ugrad.cse.buet.ac.bd>
2020-01-30 01:05:52 +0600

    input datatset preparation done

--- issue.py
+++ issue.py
@@ -3,15 +3,28 @@
 import csv
 import requests
 class Issue_Tracking_System_Report:
-    def __init__(self, issue_key, issue_id):
-        self.name = name
-        self.age = age
+    def __init__(self, issue_key, issue_id,description,issue_type):
+        self.issue_key = issue_key
+        self.issue_id = issue_id
+        self.description = description
+        self.issue_type = issue_type
 
 GITHUB_USER = 'Shusmoy108'
 GITHUB_PASSWORD = 'Sucharita13'
 REPO = 'apache/activemq'  # format is username/repo
 ISSUES_FOR_REPO_URL = 'https://api.github.com/repos/apache/activemq/pulls?state=closed'
 AUTH = (GITHUB_USER, GITHUB_PASSWORD)
+def create_input_data(filename):
+    with open(filename, 'r') as file:
+        reader = csv.reader(file)
+        issues=[]
+        for row in reader:
+            #rint(row)
+            if(row[0]!="Issue key" and row[0]!=''):
+                issue=Issue_Tracking_System_Report(row[0],row[1],row[3],row[4])
+                print(issue.issue_key)
+                issues.append(issue)
+        return issues
 
 def write_issues(response):
     "output a list of issues to csv"
@@ -32,7 +45,8 @@ r = requests.get(ISSUES_FOR_REPO_URL, auth=AUTH)
 csvfile = '%s-issues.csv' % (REPO.replace('/', '-'))
 csvout = csv.writer(open(csvfile, 'w'))
 csvout.writerow(('id', 'Title', 'Body', 'Created At', 'Updated At'))
-write_issues(r)
+create_input_data("ASF JIRA 2020-01-29T09_19_21+0000.csv")
+#write_issues(r)
 
 #more pages? examine the 'link' header returned
 if 'link' in r.headers:
